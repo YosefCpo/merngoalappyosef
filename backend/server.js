44ac3bf62e94b321628c41a837fb688c92faf91e
YosefCpo <cpo.yosef@yahoo.com>
2022-06-23 14:15:30 +0200

    Edit

--- backend/server.js
+++ backend/server.js
@@ -22,14 +22,14 @@ connectDB();
 app.use("/api/goals", goalRoute);
 app.use("/api/user", userRoute);
 
+const port = process.env.PORT || 5000;
+
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/build")));
+	app.use(express.static(path.reslove(__dirname, "../frontend/build")));
 
 	app.get("*", (req, res) =>
-		res.sendFile(
-			path.resolve(__dirname, "../", "frontend", "build", "index.html")
-		)
+		res.sendFile(path.resolve(__dirname, "../", "frontend/build", "index.html"))
 	);
 } else {
 	app.get("/", (req, res) => {
@@ -38,7 +38,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Listening app
-let port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`Listning in port ${port}`);
 });
