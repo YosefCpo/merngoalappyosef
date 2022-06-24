609200a6faacc94829210f8dbea4dee355653654
YosefCpo <cpo.yosef@yahoo.com>
2022-06-24 13:32:34 +0200

    Fixing some bugs

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({path: "../.env"});
 const cors = require("cors");
 const goalRoute = require("./routes/goalRoutes");
 const userRoute = require("./routes/userRoutes");
 const {connectDB} = require("./config/db");
 const colors = require("colors");
-
-require("dotenv").config();
 
 // Creating App
 const app = express();
@@ -26,7 +26,7 @@ const port = process.env.PORT || 5000;
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.reslove(__dirname, "../frontend/build")));
+	app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
 	app.get("*", (req, res) =>
 		res.sendFile(path.resolve(__dirname, "../", "frontend/build", "index.html"))
